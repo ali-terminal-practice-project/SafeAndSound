8b576d2ae8479bacd0794271af641e6f1e960381
Viqen <98165487+Viqen@users.noreply.github.com>
2022-10-06 20:07:40 +0800

    Add files via upload
    Update two folders of hotel and ticket

---
+++ SafeAndSound/App.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View, Text ,Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Index from './src/start'
+
+  
+  
+  function App() {
+    return (
+      <NavigationContainer>
+       <Index />
+      </NavigationContainer>
+    );
+  }
+
+export default App;
