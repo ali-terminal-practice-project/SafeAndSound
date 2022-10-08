096aa541e1628afd7bf9a92510ec406e3c57da6f
bleem <670226461@qq.com>
2022-10-08 18:54:52 +0800

    整合结束

--- src/pages/hotel/Hotel.js
+++ src/pages/hotel/Hotel.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, Button,Image } from 'react-native'
+import { Text, View, Button,Image,StyleSheet } from 'react-native'
 
 export default class Hotel extends Component {
   render() {
