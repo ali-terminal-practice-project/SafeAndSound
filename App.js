933c6eb8c318096e02529f506b970f8a17a57907
bleem <670226461@qq.com>
2022-10-08 23:57:19 +0800

    实现组件policy和tip之间的联动

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import * as React from 'react';
-import { View, Text ,Button} from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Global from './src/Global'
 // import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import Index from './src/start'
-  function App() {
-    return (
-      <NavigationContainer>
-       <Index />
-      </NavigationContainer>
-    );
-  }
+// alert(3)
+Global.params = {
+  curCity: '杭州',
+  desCity: '成都',
+   memo: ''
+}
+function App() {
+  return (
+    <NavigationContainer>
+      <Index />
+    </NavigationContainer>
+  );
+}
 
 export default App;
